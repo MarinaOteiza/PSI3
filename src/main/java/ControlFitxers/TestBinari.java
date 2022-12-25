f8aa14a62267d2e86b4e10a528669489ec96616c
sonia <soniaalfonsorivero959@gmail.com>
2022-12-25 11:09:16 +0100

    Binarios, escribir, leer y añadir Ficheros lectura pasando a lista Lista escritura a fichero Classe ModificarBinario

--- src/main/java/ControlFitxers/TestBinari.java
+++ src/main/java/ControlFitxers/TestBinari.java
@@ -2,22 +2,23 @@ package ControlFitxers;
 
 import dataUsr.User;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 public class TestBinari {
     @SuppressWarnings("unused")
-    public void escribirBinario() throws IOException {
+    public void AddBinario() throws IOException {
         try {
             int i = 15;
-            FileOutputStream archivo = new FileOutputStream("usuari.bin");
+            FileOutputStream archivo = new FileOutputStream("usuari.bin", true);
+
             //OutputStream es una clase abstracta que representa la salida de escritura. //
 
-            ObjectOutputStream escritura = new ObjectOutputStream(archivo);
+            ModificarBinari escritura = new ModificarBinari(archivo);
+            /* en vez de ObjectOutputStream podremos añadir seguidamente
+            sin borrar texto
+             */
 
-            escritura.writeObject(new usuari(15, "sonia"));
+            escritura.writeObject(new User(15, "sonia"));
             System.out.println("añadido");
             escritura.close();
 
@@ -30,14 +31,52 @@ public class TestBinari {
 
     }
 
-  public User LeerBinario(){
+    public void escribirBinario() throws IOException {
+        try {
+            int i = 15;
+            FileOutputStream archivo = new FileOutputStream("usuari.bin");
+
+            //OutputStream es una clase abstracta que representa la salida de escritura. //
+
+            ObjectOutputStream escritura = new ObjectOutputStream(archivo);
+
+            escritura.writeObject(new User(15, "sonia"));
+            System.out.println("añadido");
+            escritura.close();
+
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace(System.out);
+        }
+
+
+    }
+
+
+    /* escribirBinario ha de ir antes de AddBinario, ha de existir texto para añadirlo*/
+
+    public void leerBinario() throws ClassNotFoundException {
         User u = new User();
-        
+        try {
+            FileInputStream archivo = new FileInputStream("usuari.bin");
+            ObjectInputStream lectura = new ObjectInputStream(archivo);
+            while (true) {
+                u = (User) lectura.readObject();
+                System.out.println(u.toString());
+            }
 
-
-  }
-  // el archivo binario contiene
-
-
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (EOFException e) {
+            return;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
+
+
+
+
--- src/main/java/Productes/Productes.java
+++ src/main/java/Productes/Productes.java
@@ -1,4 +1,50 @@
 package Productes;
 
+import appUsr.RegUser;
+import ControlFitxers.Data;
+
 public class Productes {
+        private int codi;
+        private String descripcion;
+        private Data oferta;
+        private RegUser usuari;
+        private String tipus;
+
+
+        public Productes(int cod, String desc, Data off, RegUser usu, String typ) {
+            codi= cod;
+            descripcion= desc;
+            oferta= off;
+            usuari= usu;
+            tipus= typ;
+
+        }
+        public Productes(){
+
+        }
+
+        public int GetCod() {
+            return codi
+                    ;
+        }
+
+        public String getDescripcion() {
+            return descripcion;
+        }
+        public Data getOferta() {
+            return oferta;
+        }
+
+        public RegUser getUsuari() {
+            return usuari;
+        }
+
+        public String getTipus() {
+            return tipus;
+        }
+
+
+
+    }
+
 }
