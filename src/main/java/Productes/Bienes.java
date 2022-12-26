bacb3ce34f465d9965fb5093718dad1ea8d48edc
Marina Oteiza <marina.oteiza@estudiants.urv.cat>
2022-12-26 18:24:19 +0100

    productos paquete

--- src/main/java/Productes/Bienes.java
+++ src/main/java/Productes/Bienes.java
@@ -1,5 +1,74 @@
-//Sonia
 package Productes;
+import ControlFitxers.Data;
+public class Bienes extends Productes{
+    private Data dataOf;
+    private Data dataInt;
+    private String descrip;
+    private int code;
+    private double ancho;
+    private double alto;
+    private double largo;
+    private double peso;
+    private int intercanvis;
+    private boolean estat;
 
-public class Bienes {
+    public Bienes (int code,String descrip,Data dataOf, Data dataInt, boolean estat, int intercanvis, double ancho, double alto,double largo,double peso){
+        super(code,descrip, dataOf, dataInt,estat,intercanvis);
+        this.alto=alto;
+        this.ancho=ancho;
+        this.largo=largo;
+        this.peso=peso;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+
+    public double getAlto() {
+        return alto;
+    }
+
+    public void setAlto(double alto) {
+        this.alto = alto;
+    }
+
+    public double getLargo() {
+        return largo;
+    }
+
+    public void setLargo(double largo) {
+        this.largo = largo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void desactivaProducte() { estat=false;}
+    /**
+     * Método que hace la copia del biem
+     *
+     * @return duplicado de la instancia del biem
+     */
+    public Productes copia() {
+        return new Bienes(code,descrip,dataOf,dataInt,estat,intercanvis,ancho,alto,largo,peso);
+    }
+    /**
+     * Método que muestra un bien
+     *
+     * @return String con toda la información
+     */
+
+    public String toString() {
+        return "Servei [dataOf="+dataOf+",dataInt="+dataInt+ ", codi=" + code+ ", activacio=" +estat+ ", intercanvis=" +intercanvis;
+    }
+
 }
--- src/main/java/Productes/Productes.java
+++ src/main/java/Productes/Productes.java
@@ -1,50 +1,113 @@
 package Productes;
-
-import appUsr.RegUser;
 import ControlFitxers.Data;
 
-public class Productes {
-        private int codi;
-        private String descripcion;
-        private Data oferta;
-        private RegUser usuari;
-        private String tipus;
+public abstract class Productes {
+    private Data dataOf; //fecha de la oferta
+    private Data dataInt; //fecha del intercambio
+    private String descrip;   //descripcion del producto
 
+    private int code;        //codigo del producto
+    private boolean estat;
+    private int intercanvis;
 
-        public Productes(int cod, String desc, Data off, RegUser usu, String typ) {
-            codi= cod;
-            descripcion= desc;
-            oferta= off;
-            usuari= usu;
-            tipus= typ;
-
-        }
-        public Productes(){
-
-        }
-
-        public int GetCod() {
-            return codi
-                    ;
-        }
-
-        public String getDescripcion() {
-            return descripcion;
-        }
-        public Data getOferta() {
-            return oferta;
-        }
-
-        public RegUser getUsuari() {
-            return usuari;
-        }
-
-        public String getTipus() {
-            return tipus;
-        }
-
-
-
+    public Productes(int code, String descrip, Data dataOf, Data dataInt, boolean estat, int intercanvis) {
+        this.code = code;
+        this.descrip = descrip;
+        this.dataOf = dataOf;
+        this.dataInt = dataInt;
+        this.estat = true;              //el producto esta disponible
+        this.intercanvis = 0;          //no se ha hecho ningun intercambio
     }
+
+    /**
+     * getter que obtiene la fecha de intercambio de un producto
+     *
+     * @return dataInt
+     */
+    public Data getDataInt() {
+        return dataInt;
+    }
+
+    public void setDataInt(Data dataInt) {
+        this.dataInt = dataInt;
+    }
+
+    /**
+     * getter que obtiene la descripcion de un producto
+     *
+     * @return descrip
+     */
+    public String getDescrip() {
+        return descrip;
+    }
+
+    public void setNom(String nom) {
+        this.descrip = nom;
+    }
+
+    /**
+     * getter que obtiene al fecha de al oferta
+     *
+     * @return DataOf
+     */
+    public Data getDataOf() {
+        return dataOf;
+    }
+
+    public void setDataOf(Data dataOf) {
+        this.dataOf = dataOf;
+    }
+
+    public boolean isEstat() {
+        return estat;
+    }
+
+    public void setEstat(boolean estat) {
+        this.estat = estat;
+    }
+
+    /**
+     * getter que obtiene el numero de intercambios de un producto
+     *
+     * @return intercanvis
+     */
+    public int getIntercanvis() {
+        return intercanvis;
+    }
+
+    public void setIntercanvis(int intercanvis) {
+        this.intercanvis = intercanvis;
+    }
+
+    /**
+     * getter que obte el codigo de un producto
+     *
+     * @return code
+     */
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    /**
+     * Métode que desactiva un producto
+     */
+    public abstract void desactivaProducte();
+
+    /**
+     * Método que hace la copia del producto
+     *
+     * @return duplicado de la instancia de Productes
+     */
+    public abstract Productes copia();
+
+    /**
+     * Método que muestra el producto
+     */
+    public abstract String toString();
+
 
 }
